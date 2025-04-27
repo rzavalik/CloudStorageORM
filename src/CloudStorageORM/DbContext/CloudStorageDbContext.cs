46d3b8747f142ea7f3e82b43998b4b103b7ac57a
Rodrigo Castro <zavalik@live.com>
2025-04-27 18:10:59 -0300

    Closes #8
    Add CloudStorageDbContext class with options support
    Introduces the `CloudStorageDbContext` class in the
    `CloudStorageORM.DbContext` namespace. This class includes
    a private field for `CloudStorageOptions`, initialized
    through the constructor, and adds the necessary using
    directive for `CloudStorageORM.Options`.

---
+++ src/CloudStorageORM/DbContext/CloudStorageDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CloudStorageORM.Options;
+
+namespace CloudStorageORM.DbContext
+{
+    public class CloudStorageDbContext
+    {
+        private readonly CloudStorageOptions _options;
+
+        public CloudStorageDbContext(CloudStorageOptions options)
+        {
+            _options = options;
+        }
+    }
+}
