5013167831e9a88a37d092a54b26a3d5d47c8139
Rodrigo Castro <zavalik@live.com>
2025-04-27 18:06:07 -0300

    Update license and add cloud provider options
    - Changed `PackageLicenseExpression` to `GPL-3.0-or-later`.
    - Introduced `CloudProvider` enum in `CloudProvider.cs` for Azure, AWS, and Google.
    - Added `CloudStorageOptions` class in `CloudStorageOptions.cs` for configuration with properties for provider, connection string, and container name.

---
+++ src/CloudStorageORM/Options/CloudStorageOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudStorageORM.Options
+{
+    public class CloudStorageOptions
+    {
+        public Enums.CloudProvider Provider { get; set; }
+        public string ConnectionString { get; set; }
+        public string ContainerName { get; set; }
+    }
+}
